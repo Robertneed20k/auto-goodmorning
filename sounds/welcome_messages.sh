f193eba88a9515fb7ac93d60bd4249b9af0ca97b
Robertneed20k <121648505+Robertneed20k@users.noreply.github.com>
2024-06-16 21:50:35 +0800

    Update welcome_messages.sh

--- sounds/welcome_messages.sh
+++ sounds/welcome_messages.sh
@@ -29,5 +29,3 @@ random_message=${welcome_messages[$RANDOM % ${#welcome_messages[@]}]}
 
 # Display the random message using termux-toast
 termux-toast "$random_message"
-
-bash /data/data/com.termux/files/home/bin/sms
